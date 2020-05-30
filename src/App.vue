efae6a5dd97174690eb67b39d78995cd97dc4bca
林振辉台式 <281185344@qq.com>
2020-05-30 20:59:11 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from "./components/component/Header.vue";
+import Footer from "./components/component/Footer.vue";
 export default {
+  components: {
+    Header: Header,
+    Footer: Footer
+  },
   name: "App"
 };
 </script>
